349914700c342da1088bc10360a53713c4c1ad3a
Yichao 'Peak' Ji <peakji@me.com>
2022-11-19 21:04:49 +0800

    feat(chrome): record absolute coordinates

--- internal/chrome/hydrus.js
+++ internal/chrome/hydrus.js
@@ -3,6 +3,13 @@
     const AnchorHrefMaxLength = 2048
     const AnchorRelMaxLength = 128
 
+    const RegExpWhitespace = /\s+/g
+    const RegExpSpaceTab = /[ \t]+/g
+    const RegExpCRLF = /\s*[\r\n]+\s*/g
+    const RegExpComma = /,+/g
+
+    const QuerySelectorAnchor = "a[href]"
+
     // parseUrl parses URL strings into absolute URLs with search parameters
     // sorted and IDNs represented in punycode.
     function parseUrl(urlString) {
@@ -14,13 +21,13 @@
     // normalizeWhitespace replaces multiple spaces and tabs with a single
     // space character and replaces multiple CR/LFs with a single line feed.
     function normalizeWhitespace(str) {
-        return str.replace(/[ \t]+/g, " ").replace(/\s*[\r\n]+\s*/g, "\n").trim()
+        return str.replace(RegExpSpaceTab, " ").replace(RegExpCRLF, "\n").trim()
     }
 
     // squashWhitespace replaces multiple whitespace characters with a single
     // space character to ensure the returned string is a one-liner.
     function squashWhitespace(str) {
-        return str.replace(/\s+/g, " ").trim()
+        return str.replace(RegExpWhitespace, " ").trim()
     }
 
     // makeAnchor creates a new anchor record from an anchor element.
@@ -30,17 +37,17 @@
         if (!!articleElem && articleElem.contains(elem)) {
             flags.push("inarticle")
         }
-        if (squashWhitespace((elem.getAttribute("target") || "")).toLowerCase() == "_blank") {
+        if (squashWhitespace((elem.getAttribute("target") ?? "")).toLowerCase() == "_blank") {
             flags.push("newtab")
         }
         return {
-            text: squashWhitespace(elem.textContent || "") || squashWhitespace((elem.getAttribute("title") || "")),
-            href: parseUrl((elem.getAttribute("href") || "")).href,
-            rel: squashWhitespace((elem.getAttribute("rel") || "").replace(/,+/g, " ")).toLowerCase(),
+            text: squashWhitespace(elem.textContent ?? "") || squashWhitespace((elem.getAttribute("title") ?? "")),
+            href: parseUrl((elem.getAttribute("href") ?? "")).href,
+            rel: squashWhitespace((elem.getAttribute("rel") ?? "").replace(RegExpComma, " ")).toLowerCase(),
             flags: flags.join(" "),
             rect: {
-                x: Math.round(rect.x),
-                y: Math.round(rect.y),
+                x: Math.round(rect.x + window.scrollX),
+                y: Math.round(rect.y + window.scrollY),
                 width: Math.round(rect.width),
                 height: Math.round(rect.height)
             }
@@ -67,9 +74,31 @@
         return true
     }
 
-    // TODO(peakji)
-    let anchors = Array.from(document.body.querySelectorAll("a[href]")).map(x => makeAnchor(x)).filter(validateAnchor)
-    return {
-        anchors: anchors
+    function collectMeta() {
+        return {
+            title: document.title,
+            width: document.documentElement.scrollWidth,
+            height: document.documentElement.scrollHeight,
+            charset: document.characterSet
+        }
     }
+
+    function collectAnchors(elem) {
+        let a = Array.from(elem.querySelectorAll(QuerySelectorAnchor))
+        a = a.map(x => makeAnchor(x))
+        a = a.filter(validateAnchor)
+        return a
+    }
+
+    // TODO(peakji)
+    // Use the '?.' operator while chaining:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+    return {
+        meta: collectMeta(),
+        anchors: collectAnchors(document.body)
+    }
+
+    // ----
+    // Readability starts here...
+
 })()
